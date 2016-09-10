d8e10983d4e03e63460cb96632bb4d38aec2de17
Paul Tangen <ptangen@ptangen.com>
2016-09-10 16:39:04 -0400

    Review Optionals and Functions readme.

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -47,4 +47,5 @@ func produceFullName(withFirstName firstName: String, middleName: String?, lastN
 }
 
 produceFullName(withFirstName: "Jon", middleName: "Bon", lastName: "Jovi")
+
 produceFullName(withFirstName: "Mary", middleName: nil, lastName: "Doe")
